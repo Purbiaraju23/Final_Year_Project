a1f338bd08958512539c1bd52d3823dfa97863a7
Purbiaraju23 <rajupurbia978@gmail.com>
2024-03-19 08:46:48 +0530

    Blog app pages created and routing is configured

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,17 @@
-import Header from "./Header/Header"
-import Footer from "./Footer/Footer"
-import Container from "./container/Container"
-import Logo from "./Logo"
-import LogoutBtn from "./Header/LogoutBtn"
-import RTE from "./RTE"
-import Select from "./Select"
-
+import Select from "./Select";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "../components/postform/PostForm"
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
 export {
     Header,
     Footer,
@@ -13,5 +19,12 @@ export {
     Logo,
     LogoutBtn,
     RTE,
-    Select
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    Select,
 }
